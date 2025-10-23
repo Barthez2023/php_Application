5436e0caa878c4bf84b3e5706e99a3dde53d5556
Barthezpro <bartheztonlio@gmail.com>
2025-10-23 22:33:35 +0300

    Initialisation au langage php

---
+++ essai/essai.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <P>ceci et un paragfraphe en HTML</P>
+
+    <?php
+        echo "ceci a ete ecrit en php <br/>";
+        echo "Je suis un nouveau element php<br/>";
+        print "Ceci c'est un print ecrit en php";
+    ?>
+    
+</body>
+</html>
